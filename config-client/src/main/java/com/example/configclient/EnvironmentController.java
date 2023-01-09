62aeec9896dcfe0b5c5257778092d61b13b9936d
yhnoh <tedgom19@gmail.com>
2023-01-09 23:03:11 +0900

    spring cloud file 형식 셋팅

---
+++ config-client/src/main/java/com/example/configclient/EnvironmentController.java
@@ -0,0 +1,18 @@
+package com.example.configclient;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class EnvironmentController {
+
+    private final Environment environment;
+    @GetMapping("/environment")
+    public String environment(){
+
+        return environment.getProperty("environment");
+    }
+}
